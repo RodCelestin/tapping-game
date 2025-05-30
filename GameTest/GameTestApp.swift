76609cbe006ff8e49dd1730987d2e5511feac659
Rodolphe Célestin <rodolphecelestin@gmail.com>
2025-05-30 12:20:08 +0200

    Initial Commit

---
+++ GameTest/GameTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameTestApp.swift
+//  GameTest
+//
+//  Created by Rodolphe Celestin on 2025-05-30.
+//
+
+import SwiftUI
+
+@main
+struct GameTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GameTestTests/GameTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  GameTestTests.swift
+//  GameTestTests
+//
+//  Created by Rodolphe Celestin on 2025-05-30.
+//
+
+import Testing
+@testable import GameTest
+
+struct GameTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
